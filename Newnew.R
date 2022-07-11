e91312c95c769ada5b9010c4c5ecbf1759683326
YuliangWang316 <60790093+YuliangWang316@users.noreply.github.com>
2022-07-11 11:10:45 +0800

    Add files via upload

--- Newnew.R
+++ Newnew.R
@@ -23,6 +23,16 @@ Treg_CD8ratio_IFNG_PI3K_STAT3<-intersect(Treg_CD8ratio,IFNG_PI3K_STAT3)
 Treg_CD8_IFNG_PI3K_STAT3_CD8GZMB_TNF_IFNG<-intersect(Treg_CD8_IFNG_PI3K_STAT3,Treg_1c_mean_CD8GZMB_TNF_IFNG)
 Treg_CD8ratio_IFNG_PI3K_STAT3_CD8GZMB_TNF_IFNG<-intersect(Treg_CD8ratio_IFNG_PI3K_STAT3,Treg_1c_mean_CD8GZMB_TNF_IFNG)
 
+Treg_IFNG_mean<-a[which(a$d == "6" & a$o >0),]
+Treg_IFNG_mean_STAT3<-Treg_IFNG_mean[which(Treg_IFNG_mean$f == "9" | Treg_IFNG_mean$f == "10" | Treg_IFNG_mean$f == "11" | Treg_IFNG_mean$f == "12" | Treg_IFNG_mean$f == "13" | Treg_IFNG_mean$f == "14" | Treg_IFNG_mean$f == "15" | Treg_IFNG_mean$f == "16" | Treg_IFNG_mean$f == "17"),]
+Treg_IFNG_mean_PI3K<-Treg_IFNG_mean[which(Treg_IFNG_mean$f == "18" | Treg_IFNG_mean$f == "19" | Treg_IFNG_mean$f == "20" | Treg_IFNG_mean$f == "21" | Treg_IFNG_mean$f == "22" | Treg_IFNG_mean$f == "23" | Treg_IFNG_mean$f == "24" | Treg_IFNG_mean$f == "25" | Treg_IFNG_mean$f == "26"),]
+IFNGmean_PI3K_STAT3<-intersect(Treg_IFNG_mean_PI3K$p,Treg_IFNG_mean_STAT3$p)
+IFNGmean_PI3K_STAT3_Treg_CD8_IFNG_PI3K_STAT3<-intersect(IFNGmean_PI3K_STAT3,Treg_CD8_IFNG_PI3K_STAT3)
+IFNGmean_PI3K_STAT3_Treg_CD8ratio_IFNG_PI3K_STAT3<-intersect(IFNGmean_PI3K_STAT3,Treg_CD8ratio_IFNG_PI3K_STAT3)
+write.table(as.data.frame(IFNGmean_PI3K_STAT3_Treg_CD8_IFNG_PI3K_STAT3),"IFNGmean_PI3K_STAT3_Treg_CD8_IFNG_PI3K_STAT3.txt",sep = "\t")
+write.table(as.data.frame(IFNGmean_PI3K_STAT3_Treg_CD8ratio_IFNG_PI3K_STAT3),"IFNGmean_PI3K_STAT3_Treg_CD8ratio_IFNG_PI3K_STAT3.txt",sep = "\t")
+
+
 Treg_1c_median<-a[which(a$d == "4"  & a$o <0),]
 Treg_1c_median_IFNG<-Treg_1c_median[which(Treg_1c_median$f == "6" | Treg_1c_median$f == "7" | Treg_1c_median$f == "8"),]
 Treg_1c_median_STAT3<-Treg_1c_median[which(Treg_1c_median$f == "9" | Treg_1c_median$f == "10" | Treg_1c_median$f == "11" | Treg_1c_median$f == "12" | Treg_1c_median$f == "13" | Treg_1c_median$f == "14" | Treg_1c_median$f == "15" | Treg_1c_median$f == "16" | Treg_1c_median$f == "17"),]
@@ -42,5 +52,14 @@ PI3K_STAT3<-intersect(Treg_1c_sum_PI3K$p,Treg_1c_sum_STAT3$p)
 IFNG_PI3K_STAT3<-intersect(Treg_1c_sum_IFNG$p,PI3K_STAT3)
 Treg_CD8_IFNG_PI3K_STAT3<-intersect(Treg_CD8,IFNG_PI3K_STAT3)
 Treg_CD8ratio_IFNG_PI3K_STAT3<-intersect(Treg_CD8ratio,IFNG_PI3K_STAT3)
-Treg_CD8_IFNG_PI3K_STAT3_CD8GZMB_TNF_IFNG<-intersect(Treg_CD8_IFNG_PI3K_STAT3,Treg_1c_mean_CD8GZMB_TNF_IFNG)
-Treg_CD8ratio_IFNG_PI3K_STAT3_CD8GZMB_TNF_IFNG<-intersect(Treg_CD8ratio_IFNG_PI3K_STAT3,Treg_1c_mean_CD8GZMB_TNF_IFNG)
+Treg_CD8_IFNG_PI3K_STAT3_CD8GZMB_TNF_IFNG<-intersect(Treg_CD8_IFNG_PI3K_STAT3,Treg_1c_sum_CD8GZMB_TNF_IFNG)
+Treg_CD8ratio_IFNG_PI3K_STAT3_CD8GZMB_TNF_IFNG<-intersect(Treg_CD8ratio_IFNG_PI3K_STAT3,Treg_1c_sum_CD8GZMB_TNF_IFNG)
+
+Treg_IFNG_sum<-a[which(a$d == "8" & a$o >0),]
+Treg_IFNG_sum_STAT3<-Treg_IFNG_sum[which(Treg_IFNG_sum$f == "9" | Treg_IFNG_sum$f == "10" | Treg_IFNG_sum$f == "11" | Treg_IFNG_sum$f == "12" | Treg_IFNG_sum$f == "13" | Treg_IFNG_sum$f == "14" | Treg_IFNG_sum$f == "15" | Treg_IFNG_sum$f == "16" | Treg_IFNG_sum$f == "17"),]
+Treg_IFNG_sum_PI3K<-Treg_IFNG_sum[which(Treg_IFNG_sum$f == "18" | Treg_IFNG_sum$f == "19" | Treg_IFNG_sum$f == "20" | Treg_IFNG_sum$f == "21" | Treg_IFNG_sum$f == "22" | Treg_IFNG_sum$f == "23" | Treg_IFNG_sum$f == "24" | Treg_IFNG_sum$f == "25" | Treg_IFNG_sum$f == "26"),]
+IFNGsum_PI3K_STAT3<-intersect(Treg_IFNG_sum_PI3K$p,Treg_IFNG_sum_STAT3$p)
+IFNGsum_PI3K_STAT3_Treg_CD8_IFNG_PI3K_STAT3<-intersect(IFNGsum_PI3K_STAT3,Treg_CD8_IFNG_PI3K_STAT3)
+IFNGsum_PI3K_STAT3_Treg_CD8ratio_IFNG_PI3K_STAT3<-intersect(IFNGsum_PI3K_STAT3,Treg_CD8ratio_IFNG_PI3K_STAT3)
+write.table(as.data.frame(IFNGsum_PI3K_STAT3_Treg_CD8_IFNG_PI3K_STAT3),"IFNGsum_PI3K_STAT3_Treg_CD8_IFNG_PI3K_STAT3.txt",sep = "\t")
+write.table(as.data.frame(IFNGsum_PI3K_STAT3_Treg_CD8ratio_IFNG_PI3K_STAT3),"IFNGsum_PI3K_STAT3_Treg_CD8ratio_IFNG_PI3K_STAT3.txt",sep = "\t")
